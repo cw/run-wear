cb29112a0121677f68c3423b2f0a69735706bcc9
Christian Wilcox <cw@christianwilcox.com>
2016-05-29 08:39:06 -0500

    initial ajax request for lat lng data from google

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,7 +38,36 @@
   
   function logLatLon(lat, lon) {
     // get ZIP http://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&sensor=false
-    console.log("Lat:" + lat + " Lon:" + lon);
+    
+    var xmlhttp;
+
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if(xmlhttp.status == 200){
+               //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+           }
+           else if(xmlhttp.status == 400) {
+              alert('There was an error 400')
+           }
+           else {
+               alert('something else other than 200 was returned')
+           }
+        }
+    };
+
+    xmlhttp.open("GET", "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&sensor=false", true);
+    xmlhttp.send();
+    
+    console.log(xmlhttp.responseText);
+    //console.log("Lat:" + lat + " Lon:" + lon);
   }
   
   function getLocation() {
